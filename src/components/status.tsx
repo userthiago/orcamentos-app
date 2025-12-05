cbf2bbfa8ed6acd97d47ea84865cb190cc298fff
Thiago Santos <thiagosantosprofissional@gmail.com>
2025-12-05 19:25:09 -0300

    feat: adicionando componente de status

---
+++ src/components/status.tsx
@@ -0,0 +1,88 @@
+import { Lato_700Bold, useFonts } from "@expo-google-fonts/lato";
+import { StyleSheet, Text, View, ViewProps } from "react-native";
+
+type Types = "sent" | "draft" | "approved" | "declined";
+
+interface Props extends ViewProps {
+  type: Types;
+}
+
+const statusTypes: Record<
+  Types,
+  {
+    label: string;
+    style: { backgroundColor: string; labelColor: string; iconColor: string };
+  }
+> = {
+  sent: {
+    label: "Enviado",
+    style: {
+      backgroundColor: "#CEEFFF",
+      labelColor: "#1D7096",
+      iconColor: "#2AA1D9",
+    },
+  },
+  draft: {
+    label: "Rascunho",
+    style: {
+      backgroundColor: "#E6E5E5",
+      labelColor: "#676767",
+      iconColor: "#A1A2A1",
+    },
+  },
+  approved: {
+    label: "Aprovado",
+    style: {
+      backgroundColor: "#BFF7BE",
+      labelColor: "#30752F",
+      iconColor: "#4BB84A",
+    },
+  },
+  declined: {
+    label: "Recusado",
+    style: {
+      backgroundColor: "#FFD6D6",
+      labelColor: "#9E4949",
+      iconColor: "#DB4D4D",
+    },
+  },
+};
+
+export function Status({ type, ...rest }: Props) {
+  useFonts({
+    Lato_700Bold,
+  });
+
+  const { label, style } = statusTypes[type];
+
+  return (
+    <View
+      style={[styles.container, { backgroundColor: style.backgroundColor }]}
+      {...rest}
+    >
+      <View style={[styles.ellipse, { backgroundColor: style.iconColor }]} />
+      <Text style={[styles.label, { color: style.labelColor }]}>{label}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 6,
+  },
+  ellipse: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+  },
+  label: {
+    fontFamily: "Lato_700Bold",
+    fontSize: 12,
+  },
+});
